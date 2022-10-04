ec760b67bbeec2a179dcfa299bc4365b82667754
Hirisci <www.alan92@icloud.com>
2022-10-04 15:58:48 +0200

    finito

--- la-mia-pizzeria-model/Controllers/HomeController.cs
+++ la-mia-pizzeria-model/Controllers/HomeController.cs
@@ -8,10 +8,19 @@ namespace la_mia_pizzeria_model.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly List<Pizza> _pizze;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _pizze = new List<Pizza>
+            {
+                new Pizza(0, "/img/MARGHERITA-1.jpg","Margerita", "Pomodori pelati, spolverata di formaggio grattugiato e fior di latte d’Agerola", 6.00f),
+                new Pizza(1, "/img/MARGHERITA-1.jpg", "Diavola", "Pomodori pelati, salame piccante spolverata di formaggio grattugiato e fior di latte d’Agerola",  7.00f),
+                new Pizza(2, "/img/MARGHERITA-1.jpg", "Prosciutto e Funghi", "Pomodori pelati, funghi, prosciutto spolverata di formaggio grattugiato e fior di latte d’Agerola", 8.00f),
+                new Pizza(3, "/img/MARGHERITA-1.jpg", "Napoli", "Pomodori pelati, spolverata di formaggio grattugiato e fior di latte d’Agerola",  9.00f),
+                new Pizza(4, "/img/MARGHERITA-1.jpg", "Emilia", "Ragu bolognese, spolverata di formaggio grattugiato e fior di latte d’Agerola",  10.00f),
+            };
         }
 
         public IActionResult Index()
@@ -21,14 +30,13 @@ namespace la_mia_pizzeria_model.Controllers
 
         public IActionResult Menu()
         {
-            ArrayList Pizze = new ArrayList();
-            Pizze.Add(new Pizza(0, "/img/MARGHERITA-1.jpg","Margerita", "Pomodori pelati, spolverata di formaggio grattugiato e fior di latte d’Agerola", 6.00f));
-            Pizze.Add(new Pizza(1, "/img/MARGHERITA-1.jpg", "Diavola", "Pomodori pelati, salame piccante spolverata di formaggio grattugiato e fior di latte d’Agerola",  7.00f));
-            Pizze.Add(new Pizza(2, "/img/MARGHERITA-1.jpg", "Prosciutto e Funghi", "Pomodori pelati, funghi, prosciutto spolverata di formaggio grattugiato e fior di latte d’Agerola", 8.00f));
-            Pizze.Add(new Pizza(3, "/img/MARGHERITA-1.jpg", "Napoli", "Pomodori pelati, spolverata di formaggio grattugiato e fior di latte d’Agerola",  9.00f));
-            Pizze.Add(new Pizza(4, "/img/MARGHERITA-1.jpg", "Emilia", "Ragu bolognese, spolverata di formaggio grattugiato e fior di latte d’Agerola",  10.00f));
+            return View(_pizze);
+        }
 
-            return View(Pizze);
+        public IActionResult Details(int id)
+        {
+            Pizza? pizza = _pizze.Find(x=> x.Id == id);
+            return View(pizza);
         }
 
         public IActionResult Privacy()
